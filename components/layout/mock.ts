326dc5d6150239ccf35003dfe6194e5011bf7843
aaidutta <aaishik.dutta@publicissapient.com>
2023-10-18 22:40:20 +0530

    added mock data

---
+++ components/layout/mock.ts
@@ -0,0 +1,33 @@
+export const FooterMenuMock = [
+  {
+    title: 'Home',
+    path: '/'
+  },
+  {
+    title: 'About',
+    path: 'about'
+  },
+  {
+    title: 'Terms & Conditions',
+    path: 'terms-and-conditions'
+  },
+  {
+    title: 'Privacy Policy',
+    path: 'privacy-policy'
+  },
+  {
+    title: 'FAQ',
+    path: 'faq'
+  }
+];
+
+export const HeaderMenuMock = [
+  {
+    title: 'Home',
+    path: '/'
+  },
+  {
+    title: 'About',
+    path: 'about'
+  }
+];
