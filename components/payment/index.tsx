2aecab574cfc6c8f506547659a89fca6985ff602
Aaishik Dutta <aaishikdutta@Aaishiks-MacBook-Pro.local>
2024-02-20 20:14:59 +0530

    fixed payment page flickering

--- components/payment/index.tsx
+++ components/payment/index.tsx
@@ -1,39 +1,15 @@
 'use client';
 
-import { CartContext } from 'components/cart/context';
 import Footer from 'components/layout/footer';
-import { Product } from 'lib/data-types/products';
-import { useContext } from 'react';
 import OrderSummary from './order-summary';
-import PaymentDetail from './payment-detail';
 
 const Payment = () => {
-  const {state, dispatch} = useContext(CartContext)
-  let subtotal = 0
-  let delivery = 0
-  if(subtotal !== 0) {
-    delivery = 10
-  }
-
-  state?.forEach((item:Product)=> {
-    subtotal = subtotal + item?.price*item?.count || 0
-  })
+  
   return (
     <>
       <div className="mt-[-80px] bg-[linear-gradient(rgb(82,191,141),rgb(255,209,97))] text-black">
         <div className="container relative z-10 mx-auto px-[1.5rem] pt-16 lg:pt-32">
-          <div className="mx-auto flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-black md:flex-row">
-            <div className="min-h-screen w-full flex-1 md:order-none">
-              <OrderSummary
-                cartItems={state}
-                deliveryCharge={delivery}
-                subtotal={subtotal}
-              />
-            </div>
-            <div className="flex-1 md:order-last md:w-[125px]">
-              <PaymentDetail total={subtotal + delivery} />
-            </div>
-          </div>
+            <OrderSummary />
         </div>
       </div>
       <Footer />
--- components/payment/order-summary.tsx
+++ components/payment/order-summary.tsx
@@ -1,39 +1,49 @@
-import { Product } from 'lib/data-types/products';
+import { CartContext } from 'components/cart/context';
+import { useContext, useEffect, useState } from 'react';
 import OrderItem from './order-item';
 import OrderTotal from './order-total';
+import PaymentDetail from './payment-detail';
 
-const OrderSummary = ({
-  cartItems,
-  subtotal,
-  deliveryCharge
-}: {
-  cartItems: Product[];
-  subtotal: number;
-  deliveryCharge: number;
-}) => {
+const OrderSummary = () => {
+  const { state } = useContext(CartContext)
+  const [subtotal, setSubtotal] = useState(0)
+  const delivery = 0
+
+  useEffect(() => {
+    const subtotalVal = state?.reduce((total, item) => {
+      return total + (item?.price * item?.count || 0);
+    }, 0);
+    setSubtotal(subtotalVal)
+  }, [state])
   return (
-    <div>
-      <h1 className="mb-6 font-work text-[30px] font-medium leading-[36px] md:text-[35px] md:leading-[42px] xl:text-[48px] xl:leading-[56px]">
-        Order Summary
-      </h1>
-      <hr className="border-black" />
-      <div>
-        {cartItems.map((cartItem) => {
-          return (
-            <OrderItem
-              key={cartItem.id}
-              imageSrc={cartItem.image_src}
-              name={cartItem.name}
-              price={cartItem.price}
-              count={cartItem.count}
-              storeName={cartItem.store_name}
-            />
-          );
-        })}
+    <div className="mx-auto flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-black md:flex-row">
+      <div className="min-h-screen w-full flex-1 md:order-none">
+        <h1 className="mb-6 font-work text-[30px] font-medium leading-[36px] md:text-[35px] md:leading-[42px] xl:text-[48px] xl:leading-[56px]">
+          Order Summary
+        </h1>
+        <hr className="border-black" />
+        <div>
+          {state.map((cartItem) => {
+            return (
+              <OrderItem
+                key={cartItem.id}
+                imageSrc={cartItem.image_src}
+                name={cartItem.name}
+                price={cartItem.price}
+                count={cartItem.count}
+                storeName={cartItem.store_name}
+              />
+            );
+          })}
+        </div>
+        <hr className="border-black" />
+        <OrderTotal subtotal={subtotal} delivery={delivery} />
       </div>
-      <hr className="border-black" />
-      <OrderTotal subtotal={subtotal} delivery={deliveryCharge} />
+      <div className="flex-1 md:order-last md:w-[125px]">
+        <PaymentDetail total={subtotal + delivery} />
+      </div>
     </div>
+
   );
 };
 
