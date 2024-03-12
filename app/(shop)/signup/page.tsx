396ddc4e627b767c510763102a5e81a668938415
Aaishik Dutta <aaishikdutta@Aaishiks-MacBook-Pro.local>
2024-03-13 01:43:28 +0530

    fixed signup to contain address

--- app/(shop)/signup/page.tsx
+++ app/(shop)/signup/page.tsx
@@ -9,7 +9,7 @@ export const metadata = {
 };
 
 export default async function ShoppingSignupPage() {
-
+    const apiKey = process.env.GOOGLE_MAP_KEY;
     return (
         <>
             <div className="mt-[-80px] bg-[linear-gradient(rgb(82,191,141),rgb(255,209,97))] text-black">
@@ -17,7 +17,7 @@ export default async function ShoppingSignupPage() {
                     <div className="mx-auto max-w-screen-2xl px-4 h-screen">
                         <h1 className='text-center mb-5 font-work text-[30px] font-medium leading-[36px] md:text-[35px] md:leading-[42px] xl:text-[48px] xl:leading-[56px]'>Signup with Unikart Shopping</h1>
                         <div className='ml-auto mr-auto w-full max-w-[500px]'>
-                            <ShoppingSignUp />
+                            <ShoppingSignUp googleMapsApiKey={apiKey || ''} />
                         </div>
                     </div>
                 </div>
--- components/signup/shopping-signup.tsx
+++ components/signup/shopping-signup.tsx
@@ -1,27 +1,31 @@
 'use client'
 
+import { Autocomplete, useJsApiLoader } from "@react-google-maps/api";
 import LoadingDots from "components/loading-dots";
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { collection, doc, getDocs, getFirestore, query, setDoc, where } from "firebase/firestore";
 import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
 import app from "lib/firebase/init";
 import Link from "next/link";
+import { useState } from "react";
 import * as Yup from 'yup';
 
 interface SignupFormValues {
     name: string;
     email: string;
     phone: string;
+    address: string;
+    loc: { lat: number; long: number };
     password: string;
     confirmPassword: string
-  }
+}
 
 const signupSchema = Yup.object().shape({
     name: Yup.string().required("Name is required"),
     email: Yup.string().email('Invalid email').required('Email is Required'),
     phone: Yup.string()
         .matches(
-            /^(?:\+1)?(?:\([2-9]\d{2}\)\s?|[2-9]\d{2}(?:\-|\s))?[2-9]\d{2}[-\s]?\d{4}$/,
+            /^(?:\+1)?(?:\([1-9]\d{2}\)\s?|[1-9]\d{2}(?:\-|\s))?[1-9]\d{2}[-\s]?\d{4}$/,
             'Invalid phone number'
         )
         .required('Phone number is required'),
@@ -35,14 +39,21 @@ const signupSchema = Yup.object().shape({
     confirmPassword: Yup.string()
         .oneOf([Yup.ref('password'), ""], 'Passwords must match')
         .required('Confirm password is required'),
+    address: Yup.string().required("Address is required"),
 });
 
-export const ShoppingSignUp = () => {
+export const ShoppingSignUp = ({ googleMapsApiKey }: { googleMapsApiKey: string }) => {
 
-      const handleSignup = async (
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: googleMapsApiKey || '',
+        libraries: ['places']
+    });
+    const [addressAutoCompleteRef, setAddressAutoCompleteRef] = useState<google.maps.places.Autocomplete>();
+
+    const handleSignup = async (
         values: SignupFormValues,
         { setSubmitting }: FormikHelpers<SignupFormValues>
-      ) => {
+    ) => {
         try {
             const auth = getAuth(app)
             const db = getFirestore(app);
@@ -60,6 +71,8 @@ export const ShoppingSignUp = () => {
                 name: values.name,
                 email: values.email,
                 phone: values.phone,
+                address: values.address,
+                loc: values.loc,
                 type: "shopper"
             })
             alert('Signup successful!');
@@ -70,10 +83,10 @@ export const ShoppingSignUp = () => {
             alert('Something went wrong while signing up!')
         }
         setSubmitting(false);
-      };
+    };
 
-    return <Formik initialValues={{ name: '', email: '', phone: '', password: '', confirmPassword: '' }} validationSchema={signupSchema} onSubmit={handleSignup}>
-        {({ isSubmitting  }) => (
+    return isLoaded ? <Formik initialValues={{ name: '', email: '', phone: '', address: '', password: '', confirmPassword: '', loc: {lat: 0, long: 0} }} validationSchema={signupSchema} onSubmit={handleSignup}>
+        {({ isSubmitting, setFieldValue, setFieldError }) => (
             <Form>
                 <div className="mx-auto flex max-w-screen-2xl flex-col gap-8 pb-4 text-black md:flex-row">
                     <div className="min-h-screen w-full flex-1 md:order-none">
@@ -120,6 +133,37 @@ export const ShoppingSignUp = () => {
                         <div className="mb-4 flex flex-col gap-4 lg:flex-row">
                             <div className="flex-1">
                                 <label className="mb-1 block text-[14px] leading-[21px] text-black xl:text-[16px] xl:leading-[21px]">
+                                    Address
+                                </label>
+                                <Autocomplete
+                                    onLoad={(autocomplete) => setAddressAutoCompleteRef(autocomplete)}
+                                    onPlaceChanged={() => {
+                                        const place = addressAutoCompleteRef?.getPlace();
+                                        if (!place?.geometry) {
+                                            // Handle the case where no place is selected
+                                            setFieldError('address', "Cannot find the Geolocation");
+                                            return;
+                                        }
+                                        setFieldValue('address', place?.formatted_address ?? '')
+                                        const latLng = {
+                                            lat: place?.geometry?.location?.lat() ?? 0,
+                                            long: place?.geometry?.location?.lng() ?? 0
+                                        };
+                                        setFieldValue('loc', latLng);
+                                    }}
+                                >
+                                    <Field
+                                        name="address"
+                                        type="text"
+                                        className={`block w-full appearance-none rounded-[8px] bg-[rgba(0,0,0,0.26)] px-[8px] py-[12px] !placeholder-current !shadow-none !ring-transparent`}
+                                    />
+                                </Autocomplete>
+                                <ErrorMessage name="address" component="div" className="text-red-500" />
+                            </div>
+                        </div>
+                        <div className="mb-4 flex flex-col gap-4 lg:flex-row">
+                            <div className="flex-1">
+                                <label className="mb-1 block text-[14px] leading-[21px] text-black xl:text-[16px] xl:leading-[21px]">
                                     Password
                                 </label>
                                 <Field
@@ -159,5 +203,5 @@ export const ShoppingSignUp = () => {
                 </div>
             </Form>
         )}
-    </Formik>
+    </Formik>: <div>Loading ...</div>
 }
