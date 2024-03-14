a254ff84a1d2224a01b1424887258f57dc211bb1
Aaishik Dutta <aaishikdutta@Aaishiks-MacBook-Pro.local>
2024-03-14 09:13:49 +0530

    fixed hook error

--- components/layout/navbar/user.tsx
+++ components/layout/navbar/user.tsx
@@ -3,12 +3,11 @@ import { getAuth, signOut } from "firebase/auth";
 import { AuthContext } from "lib/firebase/auth";
 import app from "lib/firebase/init";
 import Link from "next/link";
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useState } from "react";
 
 export const User = ({userType = 'shopper'}: {userType?: 'shopper' | 'seller' | 'executive'}) => {
     const user = useContext(AuthContext);
     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
-    const wrapperRef = useRef<HTMLDivElement>(null); // Reference to the wrapper element
     if(user === undefined) {
         return <LoadingDots className="bg-black" />
     }
@@ -26,33 +25,16 @@ export const User = ({userType = 'shopper'}: {userType?: 'shopper' | 'seller' | 
           console.error('Logout error:', error);
         });
       };
-    
-      useEffect(() => {
-        // Function to check if clicked outside of element
-        function handleClickOutside(event: MouseEvent) {
-            if (wrapperRef.current && !wrapperRef.current.contains(event.target as Node)) {
-                setIsDropdownVisible(false); // Close the dropdown
-            }
-        }
-
-        // Bind the event listener
-        document.addEventListener("mousedown", handleClickOutside);
-
-        return () => {
-            // Unbind the event listener on clean up
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [wrapperRef]);
 
 
     if(user && user.type === userType) {
         return (
-            <div ref={wrapperRef} className="hidden rounded-[50%] border-2 border-[#ffe75f] bg-[#ffe75f] px-[16px] py-[6px] text-[20px] text-black md:block" onClick={toggleDropdown}>
+            <div className="hidden rounded-[50%] border-2 border-[#ffe75f] bg-[#ffe75f] px-[16px] py-[6px] text-[20px] text-black md:block" onClick={toggleDropdown}>
                 {user.name[0]}
                 {isDropdownVisible && (
                     <div className="dropdown-menu absolute mt-2 top-[45px] right-0 bg-white shadow-lg rounded-md py-1 w-max">
                         <p className="block px-4 py-2 text-sm text-black">Welcome, {user.name}!</p>
-                        <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="/orders">Orders</Link>
+                       {userType==='shopper' && <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="/orders">Orders</Link>}
                         <button className="dropdown-item block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" onClick={logout}>Log out</button>
                     </div>
                 )}
